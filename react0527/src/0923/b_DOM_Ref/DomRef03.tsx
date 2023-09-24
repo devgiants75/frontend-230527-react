a525b27752ff7d58d681f0d8bbe8692644046871
devgiants75 <devgiants75@naver.com>
2023-09-24 14:10:07 +0900

    20230924

--- react0527/src/0923/b_DOM_Ref/DomRef03.tsx
+++ react0527/src/0923/b_DOM_Ref/DomRef03.tsx
@@ -1,4 +1,4 @@
-import React, { InputHTMLAttributes, forwardRef, useRef } from 'react'
+import React, { forwardRef, useRef } from 'react'
 
 //! 다른 컴포넌트의 DOM 노드에 접근
 
@@ -8,6 +8,9 @@ import React, { InputHTMLAttributes, forwardRef, useRef } from 'react'
 // MyInput 컴포넌트는 forwordRef를 사용하여 선언
 // 위 Ref를 통해 inputRef에 두 번째 인수로 ref를 전달 받도록 설정
 // MyInput 자체는 받은 ref 를 내부의 input 태그에 전달
+
+// 부모 컴포넌트인 DomRef03으로부터 전달받은 ref를 내부의 
+// input 요소에 연결 가능
 const MyInput = forwardRef<HTMLInputElement, React.InputHTMLAttributes<HTMLInputElement>>((props, ref) => {
   return <input {...props} ref={ref} />;
 });
@@ -16,6 +19,7 @@ export default function DomRef03() {
   // useRef를 사용하여 input 요소의 참조를 저장
   const inputRef = useRef<HTMLInputElement>(null);
 
+  // 버튼 클릭 시 input 요소에 포커스를 하는 함수를 정의
   function handleClick() {
     if (inputRef.current) {
       inputRef.current.focus();
@@ -24,7 +28,10 @@ export default function DomRef03() {
   return (
     <>
       <hr />
-      {/* MyInput 컴포넌트에 ref를 전달하여 DOM요소에 접근 */}
+      {/* 
+        MyInput 컴포넌트에 ref를 전달하여 
+        DOM요소에 접근(input 요소에 대한 참조를 얻는다.) 
+      */}
       <MyInput ref={inputRef} />
       <button onClick={handleClick}>
         Focus the input
